f641a65b0a600a6d16efa4f71fddf7ada97298a8
Izanvz <izanvillarejo2002@gmail.com>
2021-05-21 09:32:15 +0200

    V2

--- src/joc/Player.java
+++ src/joc/Player.java
@@ -17,44 +17,36 @@ public class Player {
 
     public void Attack(Player p){
 
+        System.out.println("Estadisticas");
+        
+        System.out.println(stats());
+        
+        System.out.println(p.stats());
+        
         p.hit(this.attackPoints);
-
-
+        
+        System.out.println("\n---------------------------------------------------------------------------------------------------- ");
         System.out.println("Resutado de la pelea :");
+        System.out.println(this.name+" a atacado a "+p.getName()+" con "+this.attackPoints+" de daño pero se defiende con "+p.getDefensePoints()+" puntos de armadura ");
+        System.out.println("---------------------------------------------------------------------------------------------------- ");
+        
+        System.out.println(stats());
 
-        System.out.println(this.name);
-        stats();
-
-        System.out.println(getName());
-        p.stats();
+        System.out.println(p.stats());
 
     }
 
 
     protected void hit(int attackPoints) {
 
-        System.out.println(stats());
 
         int dmg = 0;
         dmg = this.attackPoints - getDefensePoints();
         int lifeF = getLife();
 
-
-        System.out.println(this.name+" a atacado a "+getName()+" con "+this.attackPoints+" de daño pero se defiende con "+getDefensePoints()+" puntos de armadura ");
-        System.out.print(dmg+" - "+getLife()+" = ");
         lifeF -= dmg;
-        System.out.print(lifeF);
-
+        
         setLife(lifeF);
-
-        System.out.println(stats());
-
-        if(lifeF > 0) {
-            System.out.println("Ha sido golpedo con " + dmg + " de daño");
-        }
-        if(lifeF <= 0){
-            System.out.println("Ha sido eliminado tras ser golpeado con "+dmg+" y perder toda tu vida");
-        }
 
     }
 
